f4113dda9744eda84589a5e4b8308fc35215aea6
Abhay Kumar <abhay.kumar@winni.in>
2023-10-31 21:11:26 +0530

    first commit

--- src/components/StepTwo.js
+++ src/components/StepTwo.js
@@ -1,35 +1,37 @@
 import React from "react";
 
 const StepTwo = ({ numbers, setNumbers, removeNumberField }) => {
-  const handleNumberChange = (index, value) => {
-    const updatedNumbers = [...numbers];
-    updatedNumbers[index] = value;
-    setNumbers(updatedNumbers);
-  };
-
   return (
     <div>
-      <h2>Step 2: Input Numbers</h2>
+      <h2>Step 2: Users Input Numbers</h2>
       {numbers.map((num, index) => (
-        <div key={index} className="form-group">
-          <input
-            type="text"  // Change the input type to "text"
-            className="form-control"
-            placeholder="Enter a number"
-            value={num}
-            onChange={(e) => handleNumberChange(index, e.target.value)}
-          />
-          <button
-            className="btn btn-danger"
-            onClick={() => removeNumberField(index)}
-          >
-            Remove
-          </button>
+        <div className="row">
+          <div key={index} className="form-group">
+            <input
+              type="number"
+              className="form-control col-md-12"
+              placeholder="Enter a number"
+              value={num}
+              onChange={(e) => {
+                const updatedNumbers = [...numbers];
+                updatedNumbers[index] = parseFloat(e.target.value);
+                setNumbers(updatedNumbers);
+              }}
+            />
+          </div>
+          <div className="col-md-4">
+            <button
+              className="btn btn-danger"
+              onClick={() => removeNumberField(index)}
+            >
+              Remove
+            </button>
+          </div>
         </div>
       ))}
       <button
-        className="btn btn-primary"
-        onClick={() => setNumbers([...numbers, '0'])}
+        className="btn btn-info"
+        onClick={() => setNumbers([...numbers, 0])}
       >
         Add Number
       </button>
